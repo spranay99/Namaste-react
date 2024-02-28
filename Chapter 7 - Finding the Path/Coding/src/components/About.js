bf16ef94589bd4e2b865d5a35863a6ed163181e7
test <pranayshah2206@gmail.com>
2024-02-28 17:40:13 +0530

    Created routes based on restaurants

---
+++ Chapter 7 - Finding the Path/Coding/src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <>
+      <h1>About us</h1>
+      <h2>This is Namaste React web series</h2>
+    </>
+  );
+};
+
+export default About;
---
+++ Chapter 7 - Finding the Path/Coding/src/components/Header.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [login, setLogin] = useState(false);
+  return (
+    <nav>
+      <div className="header">
+        <div className="logo-container">
+          <img className="logo" src={LOGO_URL} />
+        </div>
+
+        <div className="nav-items">
+          <ul>
+            <li>
+              <Link to="/"> Home</Link>
+            </li>
+            <li>
+              <Link to="/about"> About</Link>
+            </li>
+            <li>
+              <Link to="/contact"> Contact</Link>
+            </li>
+            <li>Cart</li>
+            <button className="filter-btn" onClick={() => setLogin(!login)}>
+              {login ? "Log out" : "Log in"}
+            </button>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
