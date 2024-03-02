cc16b0cb19dcad940e30bb84951d2cbe06201b00
test <pranayshah2206@gmail.com>
2024-03-02 16:43:14 +0530

    Chapter 8 - Lets get Classy

---
+++ Chapter 8 - Lets get Classy/Coding/src/components/RestaurantMenu.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const RestaurantMenu = () => {
+  const [menu, setMenu] = useState([]);
+  const { resId } = useParams();
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=" +
+        resId
+    );
+    const json = await response.json();
+    setMenu(json.data);
+  };
+
+  const { name, cuisines, costForTwoMessage } =
+    menu?.cards[2]?.card?.card?.info;
+
+  const { itemCards } =
+    menu?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+
+  //   console.log(itemCards);
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <h3>
+        {cuisines} - {costForTwoMessage}
+      </h3>
+      <br />
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - Rs {item.card.info.price / 100}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
